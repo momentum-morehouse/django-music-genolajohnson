c5bcdc4e1d17013f74f75bb1fcce2609c01cb8e6
Genola Johnson <genolajohnson@gmail.com>
2020-07-14 20:49:42 +0000

    started css

--- mymusic/models.py
+++ mymusic/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 
-
-
 # Create your models here.
 # class User(AbstractUser):
 #     pass
 
 class Album(models.Model):
-    artist_name = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
+    artist_name = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=255,null=True, blank=True)
     released = models.DateField()
     
     def __str__(self):
-        return f"{self.title} by {self.artist}"
+        return f"{self.title} by {self.artist_name}"
     
         # return f'{self.release}"
         
--- mymusic/views.py
+++ mymusic/views.py
@@ -4,19 +4,8 @@ from .models import Album
 # Create your views here.
 def list_albums(request):
     albums = Album.objects.all()
-    return render(request, "albums/list_albums.html",
-                  {"albums": albums})
-
-# def add_album(request):
-#     if request.method == 'GET':
-#         form = ArtistAlbum()
-#     else:
-#         form = ArtistAlbum(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect(to='list_artist')
-
-#     return render(request, "artists/add_artist.html", {"form": form})   
+    return render(request, "albums/list_albums.html",       {"albums": albums})
+  
 
 def release_date(request):
     release = Release.objects.all()
