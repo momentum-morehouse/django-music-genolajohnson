6aee179ee2926c8b8dd8855e89ab1f2156dde6f8
Genola Johnson <genolajohnson@gmail.com>
2020-07-15 22:51:44 +0000

    input image url

--- mymusic/models.py
+++ mymusic/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Album(models.Model):
     artist_name = models.CharField(max_length=255, null=True, blank=True)
+    image_url = models.TextField(null= True, blank= True)
     title = models.CharField(max_length=255,null=True, blank=True)
     released = models.DateField()
     
