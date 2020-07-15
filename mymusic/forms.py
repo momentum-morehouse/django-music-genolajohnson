10b4a021b69a895ff43b78b228aab11537bd0c16
Genola Johnson <genolajohnson@gmail.com>
2020-07-15 22:36:28 +0000

    updated 6:40

---
+++ mymusic/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Album
+
+class albumsForm(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = [
+            'title',
+            'artist_name',
+            'released',  
+            'image_source',
+        ]
